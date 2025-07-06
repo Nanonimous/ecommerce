b5937f958fd4f9e216a868218352d2656eadd7c0
Nanonimous <COCNIVAAS@GMAIL.COM>
2025-07-06 18:49:50 +0530

    browse range componeent

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/NavBar/Navbar.jsx';
 import MainSection from './components/MainSection/MainSection.jsx';
 import BrowseRange from './components/BrowseRange/BrowseRange.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar/>
       <MainSection/>
       <BrowseRange/>
+      <Footer/>
     </>
   )
 }
--- src/components/BrowseRange/BrowseRange.jsx
+++ src/components/BrowseRange/BrowseRange.jsx
@@ -1,8 +1,31 @@
 import React from "react";
+import br from './BrowseRange.module.css';
+
+import din from "../../assets/BrowseRange/living.png";
 export default function BrowseRange(){
   return(
     <>
-    <h1>browerRange</h1>
+    <div className={br.mainContainer}>
+      <div className={br.title}>
+        <h1>Brower The Range</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis </p>
+      </div>
+      <div className={br.container}>
+        <div className={br.dining}>
+          <img src={din} className={br.image} alt="" />
+          <h3>Dining</h3>
+        </div>
+        <div className={br.dining}>
+          <img src={din} className={br.image} alt="" />
+          <h3>Living</h3>
+        </div>
+        <div className={br.dining}>
+          <img src={din} className={br.image} alt="" />
+          <h3>Bed Room</h3>
+        </div>
+      </div>
+    </div>
+
     </>
   )
 }
