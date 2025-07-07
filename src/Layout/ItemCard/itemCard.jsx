2e17ad2e76d06096e7661bc0c4ca2017f04db9ba
hirthickgowtham <gowthamhirthick@gmail.com>
2025-07-06 22:55:58 -0600

    our Product  component created

--- src/Layout/ItemCard/itemCard.jsx
+++ src/Layout/ItemCard/itemCard.jsx
@@ -1,12 +1,73 @@
 import style from './ItemCard.module.css'
 
 
-export default function ItemCard() {
-    
+export default function ItemCard({item}) {
+    const {title,dis,dis_price,price,arival,content} = item;
+    console.log(title);
 
     return(
         <>
-        card
+        <div className={style.card_cover}>
+            <div className={style.image}>
+                <img src="/images/OurProductComponent/furniture1.png" alt="furniture1 image" />
+            </div>
+
+            <div className={style.content}>
+                <div className={style.title}>
+                    <h3>{title}</h3>
+                </div>
+
+                <div className={style.description}>
+                    <h4>{dis}</h4>
+                </div>
+                <div className={style.price_continer}>
+                    <div className={style.discount}>
+                        RP {dis_price}
+                    </div>
+
+                    <div className={style.original}>
+                        {price != "" ?`RP ${price}`:""}
+                    </div>
+                </div>
+            </div>
+
+
+            {
+                arival?
+                <div className={style.notation}>
+                    <p>{content}</p>
+                </div>:<div className={style.dis_notation}>
+                    <p>{`-${content}%`}</p>
+                </div>
+            }
+            
+
+            <div className={style.layer}>
+            </div>
+
+
+            <div className={style.add_cart_continer}>
+
+                <div className={style.add_cart}>
+                    <p>Add to cart</p>
+                </div>
+
+                <div className={style.options}>
+                        <div className={style.ops}>
+                            <p>Share</p>
+                        </div>
+                        <div className={style.ops}>
+                            <p>Compare</p>
+                        </div>
+                        <div className={style.ops}>
+                            <p>Like</p>
+                        </div>
+                </div>
+            </div>
+
+
+        </div>
+
         </>
     )
 }
--- src/components/Home/OurProduct/OurProduct.jsx
+++ src/components/Home/OurProduct/OurProduct.jsx
@@ -4,10 +4,63 @@ import style from  './OurProduct.module.css'
 
 export default function OurProduct() {
 
+
+    const content = [
+        {
+            title:"Syltherine",
+            dis:"Stylish cafe chair",
+            dis_price:"2,500.000",
+            price:"3,500.000",
+            arival:true,
+            content:"New"
+        },
+         {
+            title:"Syltherine",
+            dis:"Stylish cafe chair",
+            dis_price:"2,500.000",
+            price:"3,500.000",
+            arival:false,
+            content:"50"
+        },
+         {
+            title:"Syltherine",
+            dis:"Stylish cafe chair",
+            dis_price:"2,500.000",
+            price:"",
+            arival:true,
+            content:"New"
+        },
+         {
+            title:"Syltherine",
+            dis:"Stylish cafe chair",
+            dis_price:"2,500.000",
+            price:"3,200.000",
+            arival:false,
+            content:"30"
+        },
+         {
+            title:"Syltherine",
+            dis:"Stylish cafe chair",
+            dis_price:"2,500.000",
+            price:"3,500.000",
+            arival:false,
+            content:"20"
+        }
+    ]
+
     return (
         <>
-        our product
-        <ItemCard/>
+        <div className={style.product_component}>
+            <div className={style.title}>
+                <h1>Our Products</h1>
+            </div>
+
+            <div className={style.Card_continer}>
+                {content.map((item,index)=>(
+                    <ItemCard item={item} key={index} />
+                ))}
+            </div>
+        </div>
         </>
     )
     
